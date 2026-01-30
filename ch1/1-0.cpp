9f68a2c2968dfd5a2785d2fa9faceb6e23f0547f
Gary Leutheuser <gary@Garys-MacBook-Pro.local>
2026-01-29 20:21:44 -0700

    Chapter 1

---
+++ ch1/1-0.cpp
@@ -0,0 +1,35 @@
+// ask for a person's name, and greet the person
+#include <iostream>
+#include <string>
+
+int main() {
+    // ask for the person's name
+    std::cout << "Please enter your first name: ";
+
+    // read the name
+    std::string name; // define name
+    std::cin >> name; // read into name
+
+    // build the message we intend to write
+    const std::string greeting = "Hello, " + name + "!";
+
+    // build the second and fourth lines of the output
+    const std::string spaces(greeting.size(), ' ');
+    const std::string second = "* " + spaces + " *";
+
+    // build the first and fifth lines of the output
+    const std::string first(second.size(), '*');
+
+    int a = 5;
+    int a(5);
+
+    // write it all
+    std::cout << std::endl;
+    std::cout << first << std::endl;
+    std::cout << second << std::endl;
+    std::cout << "* " << greeting << " *" << std::endl;
+    std::cout << second << std::endl;
+    std::cout << first << std::endl;
+
+    return 0;
+}
